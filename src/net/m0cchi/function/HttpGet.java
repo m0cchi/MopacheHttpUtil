28a261a7e4128c63477a76d20626fb26bc47b406
m0cchi <boom.boom.planet@gmail.com>
2016-09-06 00:35:47 +0900

    impl http get method

---
+++ src/net/m0cchi/function/HttpGet.java
@@ -0,0 +1,12 @@
+package net.m0cchi.function;
+
+
+public class HttpGet extends AbstractHttpFunction {
+	private static final long serialVersionUID = 5549156780790657742L;
+
+	@Override
+	public String getMethod() {
+		return "GET";
+	}
+
+}
