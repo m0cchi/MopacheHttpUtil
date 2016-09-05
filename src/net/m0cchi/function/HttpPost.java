f910d42515ec34b8f4dded83d6a3d25fae5c86c5
m0cchi <boom.boom.planet@gmail.com>
2016-09-06 00:36:07 +0900

    impl http post method

---
+++ src/net/m0cchi/function/HttpPost.java
@@ -0,0 +1,11 @@
+package net.m0cchi.function;
+
+public class HttpPost extends AbstractHttpFunction {
+	private static final long serialVersionUID = 9082086746630999491L;
+
+	@Override
+	public String getMethod() {
+		return "POST";
+	}
+
+}
